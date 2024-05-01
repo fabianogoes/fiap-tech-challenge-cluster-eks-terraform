b172bc3db20fbd50b13039329fda4969966abb8d
Fabiano Góes <fabianogoes@gmail.com>
2024-05-01 20:35:05 -0300

    adjustmets

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 resource "aws_eks_cluster" "tech_challenge" {
   name     = "tech_challenge_eks_cluster"
-  role_arn = var.labRole
+  role_arn = "arn:aws:iam::${var.account_id}:role/LabRole"
   version = 1.29
 
   vpc_config {
-    subnet_ids = ["${var.subnetA}", "${var.subnetB}", "${var.subnetC}"]
+    subnet_ids = ["${var.subnetA}", "${var.subnetB}"]
     security_group_ids = ["${var.sgId}"]
     endpoint_private_access = true
     endpoint_public_access  = true
@@ -13,7 +13,6 @@ resource "aws_eks_cluster" "tech_challenge" {
 
   access_config {
     authentication_mode = var.accessConfig
-    bootstrap_cluster_creator_admin_permissions = true
   }  
   
 }
